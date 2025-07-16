44986a1980b4d3a64295ec71cf23af782a328fc3
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-07-16 17:33:28 +0300

    Fixed: Use null for missing release dates

--- lidarrmetadata/sql/release_group_series.sql
+++ lidarrmetadata/sql/release_group_series.sql
@@ -5,17 +5,10 @@ SELECT
       release_group.gid as AlbumId,
       release_group.name as AlbumTitle,
       artist.gid as ArtistId,
-      COALESCE(
-        make_date(
-          release_group_meta.first_release_date_year,
-          release_group_meta.first_release_date_month,
-          release_group_meta.first_release_date_day
-        ),
-        make_date(
-          COALESCE(release_group_meta.first_release_date_year, 1),
-          COALESCE(release_group_meta.first_release_date_month, 1),
-          COALESCE(release_group_meta.first_release_date_day, 1)
-        )
+      make_date(
+        release_group_meta.first_release_date_year,
+        COALESCE(release_group_meta.first_release_date_month, 1),
+        COALESCE(release_group_meta.first_release_date_day, 1)
       ) as ReleaseDate
 
     FROM release_group
