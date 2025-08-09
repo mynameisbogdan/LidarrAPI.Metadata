8519a35699b049eac7e9e5c0c5d2c5afbfc4fd5e
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-08-09 21:01:53 +0300

    Bump python to 3.12

--- lidarrmetadata/stats.py
+++ lidarrmetadata/stats.py
@@ -2,16 +2,15 @@
 Utilities for sending stats
 """
 
-import telegraf.client
-
-
 from lidarrmetadata.config import get_config
 
 CONFIG = get_config()
 
 class TelegrafStatsClient(object):
     def __init__(self, host='localhost', port=8092):
-        self._client = telegraf.client.TelegrafClient(host=host, port=port)
+        from telegraf.client import TelegrafClient
+
+        self._client = TelegrafClient(host=host, port=port)
 
     def metric(self, key, value, tags=None):
         tags = tags or {}
