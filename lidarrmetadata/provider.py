51ef2d828efcb5a42117fab3f5431e5233398a14
Bogdan <mynameisbogdan@users.noreply.github.com>
2025-12-09 16:32:23 +0200

    Remove trailing slash from fanart requests to prevent 404s

--- lidarrmetadata/provider.py
+++ lidarrmetadata/provider.py
@@ -554,7 +554,7 @@ class TheAudioDbProvider(HttpProvider,
 
         :param api_key: fanart.tv API key
         :param base_url: Base URL of API. Defaults to
-                         webservice.fanart.tv/v3/music
+                         theaudiodb.com/api/v1/json
         :param use_https: Whether or not to use https. Defaults to True.
         """
         super().__init__('tadb', session, limiter)
@@ -679,7 +679,7 @@ class FanArtTvProvider(HttpProvider,
                        InvalidateCacheMixin):
     def __init__(self,
                  api_key,
-                 base_url='webservice.fanart.tv/v3/music/',
+                 base_url='webservice.fanart.tv/v3/music',
                  use_https=True,
                  session=None,
                  limiter=None):
@@ -838,7 +838,7 @@ class FanArtTvProvider(HttpProvider,
         if url[-1] != '/':
             url += '/'
         url += mbid
-        url += '/?api_key={api_key}'.format(api_key=self._api_key)
+        url += '?api_key={api_key}'.format(api_key=self._api_key)
         return url
 
     @staticmethod
@@ -850,7 +850,7 @@ class FanArtTvProvider(HttpProvider,
         """
         images = {'Cover': util.first_key_item(response, 'albumcover'),
                   'Disc': util.first_key_item(response, 'cdart')}
-        return [{'CoverType': key, 'Url': value['url'].replace('https', 'http')}
+        return [{'CoverType': key, 'Url': value['url']}
                 for key, value in images.items() if value]
 
     @staticmethod
@@ -864,7 +864,7 @@ class FanArtTvProvider(HttpProvider,
                   'Fanart': util.first_key_item(response, 'artistbackground'),
                   'Logo': util.first_key_item(response, 'hdmusiclogo'),
                   'Poster': util.first_key_item(response, 'artistthumb')}
-        return [{'CoverType': key, 'Url': value['url'].replace('https', 'http')}
+        return [{'CoverType': key, 'Url': value['url']}
                 for key, value in images.items() if value]
 
 class SpotifyAuthProvider(HttpProvider,
