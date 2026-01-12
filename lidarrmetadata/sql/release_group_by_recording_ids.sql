3cd0771adaf303a7cdd3bcca128a4353dcb1efbb
Bogdan <mynameisbogdan@users.noreply.github.com>
2026-01-12 21:41:32 +0200

    Hide disabled links, release labels and countries

--- lidarrmetadata/sql/release_group_by_recording_ids.sql
+++ lidarrmetadata/sql/release_group_by_recording_ids.sql
@@ -1,22 +1,24 @@
-SELECT scored.rgid as rgid, min(scored.score) as score
+SELECT scored.rgid AS rgid, MIN(scored.score) AS score
 FROM
 (
-	SELECT matches.rgid,
-	ABS(SUM(medium.track_count) - $1) + (1 - cast(max(matches.matchcount) as float) / sum(medium.track_count)) as score
-	FROM
-	(
-		SELECT release_group.gid as rgid, release.id as releaseid, count(recording.id) as matchcount
-		FROM release_group
-		JOIN release on release.release_group = release_group.id
-		JOIN medium on medium.release = release.id
-		JOIN track on track.medium = medium.id
-		JOIN recording on recording.id = track.recording
-		WHERE recording.gid = ANY($2::uuid[])
-		group by release_group.gid, release.id
-	)	as matches
-	JOIN medium on medium.release = matches.releaseid
-	GROUP BY matches.rgid, matches.releaseid
-	order by score) as scored
+  SELECT
+    matches.rgid,
+    ABS(SUM(medium.track_count) - $1) + (1 - CAST(MAX(matches.matchcount) AS float) / SUM(medium.track_count)) AS score
+  FROM
+  (
+    SELECT release_group.gid AS rgid, release.id AS releaseid, count(recording.id) AS matchcount
+    FROM release_group
+    JOIN release on release.release_group = release_group.id
+    JOIN medium on medium.release = release.id
+    JOIN track on track.medium = medium.id
+    JOIN recording on recording.id = track.recording
+    WHERE recording.gid = ANY($2::uuid[])
+    GROUP BY release_group.gid, release.id
+  ) AS matches
+  JOIN medium on medium.release = matches.releaseid
+  GROUP BY matches.rgid, matches.releaseid
+  ORDER BY score
+) AS scored
 GROUP BY scored.rgid
 ORDER BY score
 LIMIT 5
